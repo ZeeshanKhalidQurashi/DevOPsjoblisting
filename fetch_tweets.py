d31ec3550eaac6c1683142209ce5b830669bceca
ZeeshanKhalidQurashi <zeeshankhalidqurashi@gmail.com>
2024-08-07 23:23:30 +0500

    Add debugging to fetch_tweets.py and update workflow

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -1,22 +1,34 @@
+import os
 import requests
 import json
 
-API_KEY = 'your_social_searcher_api_key'
+API_KEY = os.getenv('SOCIAL_SEARCHER_API_KEY')
 query = 'DevOps remote job'
 url = f'https://api.social-searcher.com/v2/search?key={API_KEY}&q={query}&network=twitter'
 
 response = requests.get(url)
 data = response.json()
 
+# Debugging: Print the entire response
+print("API Response:")
+print(json.dumps(data, indent=2))
+
 # Extract relevant data
 tweet_list = []
-for post in data['posts']:
-    tweet_list.append({
-        'text': post['text'],
-        'created_at': post['published'],
-        'user': post['user']['name']
-    })
+if 'posts' in data:
+    for post in data['posts']:
+        tweet_list.append({
+            'text': post['text'],
+            'created_at': post['published'],
+            'user': post['user']['name']
+        })
 
-# Save to a JSON file
-with open('tweets.json', 'w') as f:
-    json.dump(tweet_list, f)
+    # Save to a JSON file
+    with open('tweets.json', 'w') as f:
+        json.dump(tweet_list, f)
+else:
+    print("No 'posts' key in the response")
+
+# Save the raw response for debugging purposes
+with open('api_response.json', 'w') as f:
+    json.dump(data, f)
